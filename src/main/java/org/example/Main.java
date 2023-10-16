c5d007af009ad183de6bacf3ec461324a0cf5d05
shashikam <shashikam@codegen.net>
2023-10-16 18:59:24 +0530

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        int a = 10;
+        int b = 12;
+        int sum = a+b;
+
+        System.out.println(sum);
     }
 }
